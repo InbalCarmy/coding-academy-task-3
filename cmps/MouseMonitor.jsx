a54b57447d947bf726525b780be31af7a8875b9d
Inbal Carmy <inbal950@gmail.com>
2025-08-25 13:32:28 +0300

    task 5

--- cmps/MouseMonitor.jsx
+++ cmps/MouseMonitor.jsx
@@ -3,10 +3,34 @@ const { useState, useEffect } = React
 
 export function MouseMonitor()  {
 
+    const [isOn, setIsOn] = useState(true)
+    const [pos, setPos] = useState({x: 0, y: 0})
+
+    useEffect(() => {
+        if(!isOn) return
+
+          function updatePos(e){
+        setPos({x: e.clientX, y: e.clientY})
+        }
+
+        document.addEventListener('mousemove', updatePos)
+
+        return () => document.removeEventListener('mousemove', updatePos)
+    }, [isOn])
+
+
+    const on = (isOn) ? '' : 'paused'
 
     return (
-        <section className="mouse-monitor">
-            <h2>Mouse Monitor</h2>
+        <section className={`mouse-monitor ${on}`}>
+            <div className="row">
+                <p>Mouse:</p>
+                <p>x: {pos.x}</p>
+                <p>y: {pos.y}</p>
+            </div>
+
+            <button className="toggle-btn" onClick={() => setIsOn(on => !on)}>{isOn ? 'Pause' : 'Resume'}</button>
+            {/* <h2>Mouse Monitor</h2> */}
        </section>
     )
 }
